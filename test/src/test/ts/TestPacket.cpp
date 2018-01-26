0a813be0164617534a6fb6e387bda6eff33dff23
Jim Knowler <jim.knowler2@gmail.com>
2018-01-26 19:46:36 +0000

    ts: Packet - implement packet format

--- test/src/test/ts/TestPacket.cpp
+++ test/src/test/ts/TestPacket.cpp
@@ -8,3 +8,99 @@ TEST(Packet, ShouldConstruct) {
 	UNUSED(packet);
 }
 
+TEST(Packet, ShouldPackHeaderToCorrectSize) {
+	ts::Packet::Header header;
+	UNUSED(header);
+
+	unsigned headerSize = sizeof(header);
+	EXPECT_EQ(4, headerSize);
+}
+
+TEST(Packet, ShouldPackAdaptationFieldHeaderToCorrectSize) {
+	ts::Packet::AdaptationFieldHeader adaptationFieldHeader;
+	UNUSED(adaptationFieldHeader);
+
+	unsigned adaptationFieldHeaderSize = sizeof(adaptationFieldHeader);
+	EXPECT_EQ(2, adaptationFieldHeaderSize);
+}
+
+TEST(Packet, ShouldPackOptionalAdaptationFieldToCorrectSizePCR) {
+	ts::Packet::PCR pcr;
+	UNUSED(pcr);
+
+	unsigned pcrSize = sizeof(pcr);
+	EXPECT_EQ(6, pcrSize);
+}
+
+TEST(Packet, ShouldPackOptionalAdaptationFieldToCorrectSizeOPCR) {
+	ts::Packet::OPCR opcr;
+	UNUSED(opcr);
+
+	unsigned opcrSize = sizeof(opcr);
+	EXPECT_EQ(6, opcrSize);
+}
+
+TEST(Packet, ShouldPackOptionalAdaptationFieldToCorrectSizeSpliceCountdown) {
+	ts::Packet::SpliceCountdown spliceCountdown;
+	UNUSED(spliceCountdown);
+
+	unsigned spliceCountdownSize = sizeof(spliceCountdown);
+	EXPECT_EQ(1, spliceCountdownSize);
+}
+
+TEST(Packet, ShouldPackOptionalAdaptationFieldToCorrectSizeTransportPrivateDataLength) {
+	ts::Packet::TransportPrivateDataLength transportPrivateDataLength;
+	UNUSED(transportPrivateDataLength);
+
+	unsigned transportPrivateDataLengthSize = sizeof(transportPrivateDataLength);
+	EXPECT_EQ(1, transportPrivateDataLengthSize);
+}
+
+TEST(Packet, ShouldPackAdaptationExtensionFieldToCorrectSize) {
+	ts::Packet::AdaptationExtensionField adaptationExtensionField;
+	UNUSED(adaptationExtensionField);
+
+	unsigned adaptationExtensionFieldSize = sizeof(adaptationExtensionField);
+	EXPECT_EQ(2, adaptationExtensionFieldSize);
+}
+
+TEST(Packet, ShouldPackLegalTimeWindowFieldToCorrectSize) {
+	ts::Packet::LegalTimeWindow legalTimeWindowField;
+	UNUSED(legalTimeWindowField);
+
+	unsigned legalTimeWindowFieldSize = sizeof(legalTimeWindowField);
+	EXPECT_EQ(2, legalTimeWindowFieldSize);
+}
+
+TEST(Packet, ShouldPackPiecewiseRateFieldToCorrectSize) {
+	ts::Packet::PiecewiseRate piecewiseRate;
+	UNUSED(piecewiseRate);
+
+	unsigned piecewiseRateSize = sizeof(piecewiseRate);
+	EXPECT_EQ(3, piecewiseRateSize);
+}
+
+TEST(Packet, ShouldPackSeamlessSpliceToCorrectSize) {
+	ts::Packet::SeamlessSplice seamlessSplice;
+	UNUSED(seamlessSplice);
+
+	unsigned seamlessSpliceSize = sizeof(seamlessSplice);
+	EXPECT_EQ(5, seamlessSpliceSize);
+}
+
+TEST(Packet, ShouldPackPacketToCorrectSize) {
+	ts::Packet packet;
+	UNUSED(packet);
+
+	unsigned packetSize = sizeof(packet);
+	EXPECT_EQ(188, packetSize);
+}
+
+TEST(Packet, ShouldDefineConstantForStuffingByte) {
+	EXPECT_EQ(0xFF, ts::Packet::kStuffingByte);
+}
+
+TEST(Packet, ShouldDefineConstantForSyncByte) {
+	EXPECT_EQ(0x47, ts::Packet::kSyncByte);
+}
+
--- ts/include/ts/Packet.h
+++ ts/include/ts/Packet.h
@@ -1,9 +1,102 @@
 #pragma once
 
+#include <stdint.h>
+
 namespace ts {
 	
 	class Packet {
 	public:
 
+		enum AdaptationFieldControl : uint32_t {
+			kPayloadOnly = 1,
+			kAdaptationFieldOnly = 2,
+			kAdaptationFieldFollowedByPayload = 3,
+			kReserved
+		};
+		
+		enum TransportScramblingControl : uint32_t {
+			kNotScrambled = 0,
+			kReservedForFutureUse = 1,
+			kScrambledWithEventKey = 2,
+			kScrambledWithOddKey = 3
+		};
+
+		struct Header {
+			uint32_t syncByte : 8;
+			uint32_t transportErrorIndicator : 1;
+			uint32_t payloadUnitStartIndicator : 1;
+			uint32_t transportPriority : 1;
+			uint32_t pid : 13;
+			TransportScramblingControl transportScramblingControl : 2;
+			AdaptationFieldControl adaptationFieldControl : 2;
+			uint32_t continuityCounter : 4;
+		};
+
+		struct AdaptationFieldHeader {
+			uint16_t adaptationFieldLength : 8;
+			uint16_t discontinuityIndicator : 1;
+			uint16_t randomAccessIndicator : 1;
+			uint16_t elementaryStreamPriorityIndicator : 1;
+			uint16_t flagPCR : 1;
+			uint16_t flagOPCR : 1;
+			uint16_t flagSplicingPoint : 1;
+			uint16_t flagTransportPrivateData : 1;
+			uint16_t flagAdaptationFieldExtension : 1;
+		};
+
+		struct PCR {
+			/// @todo 33 bits base
+			/// 6 bits reserved
+			/// 9 bits extension
+			uint8_t pcr[6];
+		};
+
+		struct OPCR : PCR {
+
+		};
+
+		struct SpliceCountdown {
+			int8_t spliceCountdown;
+		};
+
+		struct TransportPrivateDataLength {
+			uint8_t transportPrivateDataLength;
+		};
+
+		struct AdaptationExtensionField {
+			uint16_t adaptationExtensionLength : 8;
+			uint16_t flagLegalTimeWindow : 1;
+			uint16_t flagPiecewiseRate : 1;
+			uint16_t flagSeamlessSpliceFlag : 1;
+			uint16_t reserved : 5;
+		};
+
+		struct LegalTimeWindow {
+			uint16_t flagLegalTimeWindowValid : 1;
+			uint16_t legalTimeWindowOffset : 15;
+		};
+
+		struct PiecewiseRate {
+			uint8_t data[3];
+
+			/// @todo reserved : 2
+			/// @todo piecewiseRate : 22
+		};
+
+		struct SeamlessSplice {
+			uint8_t data[5];
+
+			/// @todo spliceType : 5
+			/// @todo nextAccessUnitDTS : 36
+		};
+
+		enum : uint8_t {
+			kStuffingByte = 0xff,
+			kSyncByte = 0x47
+		};
+
+		Header header;
+
+		uint8_t data[184];
 	};
 }
